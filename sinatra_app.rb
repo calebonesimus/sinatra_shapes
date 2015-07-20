07c262fff4e06b4a1fee24175b3fb3ec61407ad4
Caleb Francis <calebonesimus@gmail.com>
2015-07-20 11:01:56 -0400

    initial commit

---
+++ sinatra_app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+# Creates box with url params
+get '/box/:width/:height' do |width, height|
+  @width = width
+  @height = height
+  erb :box
+end
+
+# Creates an oval/circle with url params
+get '/circle/:width/:height' do |width, height|
+  @width = width
+  @height = height
+  erb :circle
+end
